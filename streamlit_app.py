5d888452cab92011667bac928fecab3548a0289b
Veesheen Yuen <veeyuen@gmail.com>
2023-03-24 19:06:38 +0800

    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,8 @@ import numpy as np
 from google.oauth2 import service_account
 from google.cloud import storage
 
+print('start')
+
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
