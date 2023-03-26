26d74403201e0ec2cb353bdf29c36f38368a3faa
Veesheen Yuen <veeyuen@gmail.com>
2023-03-26 10:55:55 +0800

    pandas filter streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,7 +163,7 @@ end_date = st.sidebar.selectbox('End Date', dates)
 
 mask = (data['Date'] > start_date) & (data['Date'] <= end_date)
 
-filter=data.loc[(data['Event']==event_choice) & (data.loc[mask])]
+#filter=data.loc[(data['Event']==event_choice) & (data.loc[mask])]
 
 
 #container = st.beta_container()
@@ -183,7 +183,7 @@ filter=data.loc[(data['Event']==event_choice) & (data.loc[mask])]
 #    filter=data.loc[(data['Event']==make_choice) & (data['Date']==date_choice)]
 
 
-st.dataframe(filter)
+#st.dataframe(filter)
 
 
 
