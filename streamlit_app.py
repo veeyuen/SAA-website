f47b8e71ee4f88c3daf79206d7e0efd1d585e0c1
Veesheen Yuen <veeyuen@gmail.com>
2023-03-26 12:47:49 +0800

    pandas filter streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,7 +176,9 @@ ax = sns.histplot(data=filter, x='Age', kde=True, color = "#b80606")
 
 st.pyplot(fig)
 
-summary = filter.describe(include='all').fillna("").astype("str")
+stats=filter['Age']
+
+summary = stats.describe(include='all').fillna("").astype("str")
 st.write(summary)
 
 
