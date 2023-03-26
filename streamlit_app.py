693d6a8fcfffdef64f6d97eb9962f387458537a7
Veesheen Yuen <veeyuen@gmail.com>
2023-03-26 12:51:37 +0800

    pandas filter streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ URL = ("https://storage.googleapis.com/singapore_athletics_association/consolida
 
 def load_data():
 
-    print("Loading data from Google Cloud Storage....")
+    st.write("Loading data from Google Cloud Storage....")
     client = storage.Client()
     data = pd.read_csv(URL, usecols = ['Date','Event', 'Name', 'Age', 'Team', 'Result', 'm/s', 'Competition',
               'Year D.O.B.', 'Info, if any'])
