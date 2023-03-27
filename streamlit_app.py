9a517cc1db49a43336e13eb016dc6efb18c8aea9
Veesheen Yuen <veeyuen@gmail.com>
2023-03-27 18:46:56 +0800

    GCS file upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,11 +119,11 @@ def clean(data):
 
 
 
-# Create API client.
-#credentials = service_account.Credentials.from_service_account_info(
-#    st.secrets["gcp_service_account"]
-#)
-#client = storage.Client(credentials=credentials)
+ Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
 
 # Retrieve file contents.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -167,86 +167,6 @@ def load_data():
     return data
 
 data = load_data()
-
-#st.dataframe(data)
-
-## Interactive dataframe filtering
-def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Adds a UI on top of a dataframe to let viewers filter columns
-
-    Args:
-        df (pd.DataFrame): Original dataframe
-
-    Returns:
-        pd.DataFrame: Filtered dataframe
-    """
-    #modify = st.checkbox("Add filters")
-
-    #if not modify:
-    #    return df
-
-    #df = df.copy()
-
-    # Try to convert datetimes into a standard format (datetime, no timezone)
-    for col in df.columns:
-        if is_object_dtype(df[col]):
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except Exception:
-                pass
-
-        if is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.tz_localize(None)
-
-    modification_container = st.container()
-
-    with modification_container:
-        to_filter_columns = st.multiselect("Filter dataframe on", df.columns)
-        for column in to_filter_columns:
-            left, right = st.columns((1, 20))
-            # Treat columns with < 10 unique values as categorical
-            if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-                user_cat_input = right.multiselect(
-                    f"Values for {column}",
-                    df[column].unique(),
-                    default=list(df[column].unique()),
-                )
-                df = df[df[column].isin(user_cat_input)]
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = (_max - _min) / 100
-                user_num_input = right.slider(
-                    f"Values for {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
-            elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f"Values for {column}",
-                    value=(
-                        df[column].min(),
-                        df[column].max(),
-                    ),
-                )
-                if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    start_date, end_date = user_date_input
-                    df = df.loc[df[column].between(start_date, end_date)]
-            else:
-                user_text_input = right.text_input(
-                    f"Substring or regex in {column}",
-                )
-                if user_text_input:
-                    df = df[df[column].astype(str).str.contains(user_text_input)]
-
-    return df
-
-#st.dataframe(filter_dataframe(data))
 
 
 # Filter dataframe
@@ -323,7 +243,11 @@ def upload(file):
 
 def upload_csv(df):
 
-    client = storage.Client()
+    credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+    client = storage.Client(credentials=credentials)
+
     bucket = client.get_bucket('singapore_athletics_association')
 
     df.to_csv()
@@ -336,7 +260,7 @@ def upload_csv(df):
 frames=[df_processed, data]
 
 upload_df = pd.concat(frames)
-upload_df.reset_index(drop=True)
+upload_df = upload_df.reset_index(drop=True)
 
 st.dataframe(upload_df)
 
