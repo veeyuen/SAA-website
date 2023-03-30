103b3f7adeb8a86301315b33d12ebbd4d4b810d8
Veesheen Yuen <veeyuen@gmail.com>
2023-03-30 16:14:06 +0800

    import error fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,9 @@ import pandas as pd
 import numpy as np
 import datetime
 import logging
-import seaborn as sns
-
+#import seaborn as sns
 
 from matplotlib import pyplot as plt
-
 
 from pandas.api.types import (
     is_categorical_dtype,
